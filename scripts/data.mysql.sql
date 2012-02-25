08ff9aaabe8ec808b6b54744c834505336f55a23
orbisnull <zen4dev@gmail.com>
2012-02-25 16:37:04 +0900

    resolve error in load data in database script

--- scripts/data.mysql.sql
+++ scripts/data.mysql.sql
@@ -2,5 +2,5 @@
 -- Query: 
 -- Date: 2012-02-24 00:56
 */
-INSERT INTO `entry` (`id`,`title`,`data`,`created`) VALUES (NULL,'First Entry','<p>many  words</p<','2012-02-24 00:49:49');
-INSERT INTO `entry` (`id`,`title`,`data`,`created`) VALUES (NULL,'Second Entry','<b>Bold</b> <p>Second Post</p>','2012-02-20 13:00:00');
+INSERT INTO `entry` (`id`,`title`,`body`,`created`) VALUES (NULL,'First Entry','<p>many  words</p<','2012-02-24 00:49:49');
+INSERT INTO `entry` (`id`,`title`,`body`,`created`) VALUES (NULL,'Second Entry','<b>Bold</b> <p>Second Post</p>','2012-02-20 13:00:00');
