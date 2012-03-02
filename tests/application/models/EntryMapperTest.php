be7f474644c823fea1354f506dbfcc27b95885f7
orbisnull <zen4dev@gmail.com>
2012-03-02 14:43:54 +0900

    change use phpunit 3.6 and factory

--- tests/application/models/EntryMapperTest.php
+++ tests/application/models/EntryMapperTest.php
@@ -38,36 +38,59 @@ class Application_Model_EntryMapperTest extends PHPUnit_Framework_TestCase
         $this->adapter = new Zend_Test_DbAdapter();
 
         $this->mock = $this->getMock("Application_Model_DbTable_Entry");
-        $this->mock->expects($this->any())
-            ->method("getAdapter")
-            ->will($this->returnValue($this->adapter));
+
+        $this->setUpMock();
 
         OrbisLib_DataMapperFactory::setDbTable("Application_Model_EntryMapper", $this->mock);
 
         $this->mapper = OrbisLib_DataMapperFactory::create("Application_Model_EntryMapper");
-
     }
 
-    static public function getTestRowSet($id)
+    public function setUpMock()
     {
-        $testData = array("id" => "1",
-            "title" => "First Entry",
-            "body" => "Long text",
-            "created" => "2012-02-20 15:00:00",
+        $rowsetMap = array(
+            array(0, new Zend_Db_Table_Rowset(array("table" => $this->mock, "data" => array()))),
+            array(1, new Zend_Db_Table_Rowset(array("table" => $this->mock, "data" => array($this->testData)))),
         );
 
-        switch ($id) {
-            case 1:
-                return new Zend_Db_Table_Rowset(array("table" => new Application_Model_DbTable_Entry(), "data" => array($testData)));
-                break;
-            case 0:
-                return new Zend_Db_Table_Rowset(array("table" => new Application_Model_DbTable_Entry(), "data" => array()));
-                break;
-            default:
-                return null;
-        }
-    }
+        $this->mock->expects($this->any())
+            ->method("getAdapter")
+            ->will($this->returnValue($this->adapter));
+        $this->mock->expects($this->any())
+            ->method("find")
+            ->will($this->returnValueMap($rowsetMap));
 
+        $testDataNew=$this->testData;
+        unset($testDataNew["id"]);
+        $testDataNew["created"]=date("Y-m-d h:i:s");
+
+        $testDataUpdate=$this->testData;
+        unset($testDataUpdate["created"]);
+
+        $this->mock->expects($this->any())
+            ->method("insert")
+            ->with($testDataNew)
+            ->will($this->returnValue(true));
+        $this->mock->expects($this->any())
+            ->method("update")
+            ->with($testDataUpdate, array('id = ?' => "1"))
+            ->will($this->returnValue(true));
+
+        $this->mock->expects($this->any())
+            ->method("delete")
+            ->with($this->adapter->quoteInto("id = ?", 1))
+            ->will($this->returnValue(true));
+
+        $testData2 = $this->testData;
+        $testData2["id"] = 3;
+        $testData2["title"] = "33 korovi";
+        $testRowset = new Zend_Db_Table_Rowset(array("table" => new Application_Model_DbTable_Entry(),
+            "data" => array($this->testData, $testData2)));
+
+        $this->mock->expects($this->any())
+            ->method("fetchAll")
+            ->will($this->returnValue($testRowset));
+    }
 
     public function testDbTable()
     {
@@ -90,18 +113,14 @@ class Application_Model_EntryMapperTest extends PHPUnit_Framework_TestCase
 
     public function testFind()
     {
-        $this->mock->expects($this->any())
-            ->method("find")
-            ->will($this->returnCallback("Application_Model_EntryMapperTest::getTestRowSet"));
-
         $entry = new Application_Model_Entry();
         $result = $this->mapper->find(1, $entry);
-        $this->assertTrue($result);
-        $this->assertEquals(new Application_Model_Entry($this->testData), $entry);
+        $this->assertTrue($result, "Not found entry (id=1)");
+        $this->assertEquals(new Application_Model_Entry($this->testData), $entry, "Found other entry");
 
         $entry = new Application_Model_Entry();
         $result = $this->mapper->find(0, $entry);
-        $this->assertFalse($result);
+        $this->assertFalse($result, "Found missing entry");
     }
 
     public function testSave()
@@ -112,15 +131,6 @@ class Application_Model_EntryMapperTest extends PHPUnit_Framework_TestCase
 
         $testDataUpdate=$this->testData;
         unset($testDataUpdate["created"]);
-
-        $this->mock->expects($this->any())
-            ->method("insert")
-            ->with($testDataNew)
-            ->will($this->returnValue(true));
-        $this->mock->expects($this->any())
-            ->method("update")
-            ->with($testDataUpdate, array('id = ?' => "1"))
-            ->will($this->returnValue(true));
 
          //test insert
         $testDataNew["id"]=0;
@@ -141,12 +151,6 @@ class Application_Model_EntryMapperTest extends PHPUnit_Framework_TestCase
         $testData2 = $this->testData;
         $testData2["id"] = 3;
         $testData2["title"] = "33 korovi";
-        $testRowset = new Zend_Db_Table_Rowset(array("table" => new Application_Model_DbTable_Entry(),
-            "data" => array($this->testData, $testData2)));
-
-        $this->mock->expects($this->any())
-            ->method("fetchAll")
-            ->will($this->returnValue($testRowset));
 
         $entris = array(new Application_Model_Entry($this->testData)
         , new Application_Model_Entry($testData2)
@@ -157,10 +161,6 @@ class Application_Model_EntryMapperTest extends PHPUnit_Framework_TestCase
 
     public function testDelete()
     {
-        $this->mock->expects($this->any())
-            ->method("delete")
-            ->with($this->adapter->quoteInto("id = ?", 1))
-            ->will($this->returnValue(true));
 
         $entry = new Application_Model_Entry($this->testData);
 
